0e70ca00ed11ce941a47c57f287a710bf1a4f081
Shivansh Kumar <shivansh.dbg@gmail.com>
2019-11-22 12:18:01 +0530

    2nd change

--- src/main/java/com/infy/demos/maven_quickstart/App.java
+++ src/main/java/com/infy/demos/maven_quickstart/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello shivansh! This is a maven quickstart project." );
+	int x = 5;
+	int y = 6;
+	int z;
+	z = x + y;
+	z = x + y;
+	System.out.println( "The value of " + x + " + " + y + " = " + z );
     }
 }
