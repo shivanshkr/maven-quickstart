9d1631016585f629dde81729dab810689b8f8885
Shivansh Kumar <shivansh.dbg@gmail.com>
2019-11-22 12:01:52 +0530

    first commit

---
+++ src/main/java/com/infy/demos/maven_quickstart/App.java
@@ -0,0 +1,13 @@
+package com.infy.demos.maven_quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello shivansh! This is a maven quickstart project." );
+    }
+}
